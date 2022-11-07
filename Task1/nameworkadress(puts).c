92d1ab729bccc4c2fdcf9f00b17260dc3d147cc9
SybilNoMercy <113617788+SybilNoMercy@users.noreply.github.com>
2022-11-07 22:07:21 +0300

    Update nameworkadress(puts).c

--- Task1/nameworkadress(puts).c
+++ Task1/nameworkadress(puts).c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-char info[100];
-printf("Enter your name, workplace, adress :\n");
-fgets(info, 100, stdin);
-printf("Your info:");
-puts(info);
-return 0;
+    char info[100];
+    printf("Enter your name, workplace, adress :\n");
+    fgets(info, 100, stdin);
+    printf("Your info:");
+    puts(info);
+    return 0;
 }

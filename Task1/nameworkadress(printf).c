25b1673bc9b0b83848d09670890085d72085866b
SybilNoMercy <113617788+SybilNoMercy@users.noreply.github.com>
2022-11-07 22:06:48 +0300

    Update nameworkadress(printf).c

--- Task1/nameworkadress(printf).c
+++ Task1/nameworkadress(printf).c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-char info[100];
-printf("Enter your name, workplace, adress :\n");
-fgets(info, 100, stdin);
-printf("Your info: %s", info);
-return 0;
+    char info[100];
+    printf("Enter your name, workplace, adress :\n");
+    fgets(info, 100, stdin);
+    printf("Your info: %s", info);
+    return 0;
 }
